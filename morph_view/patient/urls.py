d38899208d35613f37d147cb970679a88b726599
max31ru12 <maxevg72@gmail.com>
2023-02-27 18:09:18 +0300

    fixed urls and views

--- morph_view/patient/urls.py
+++ morph_view/patient/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-    path('', patinet_index, name='patient-main-page')
+    path('', patient_index, name='patient-main-page')
 ]
--- morph_view/patient/views.py
+++ morph_view/patient/views.py
@@ -2,5 +2,5 @@ from django.shortcuts import render
 
 
 # Create your views here.
-def patinet_index(request):
+def patient_index(request):
     return render(request, 'patient/base.html')
