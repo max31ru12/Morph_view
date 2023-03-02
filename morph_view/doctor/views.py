36eb37da2003dcad2cc9654771c53242207c20c9
max31ru12 <maxevg72@gmail.com>
2023-03-02 21:41:32 +0300

    modified patient's and mainapp's template's media-queries and configurated new app

--- morph_view/doctor/views.py
+++ morph_view/doctor/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from django.http.response import HttpResponse
 
 # Create your views here.
+
+
+def doctor_view(request):
+    return HttpResponse('<h1>Это страница дл врачей</h1>')
--- morph_view/morph_view/urls.py
+++ morph_view/morph_view/urls.py
@@ -21,5 +21,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('mainapp.urls')),
     path('patient/', include('patient.urls')),
-
+    path('doctor/', include('doctor.urls')),
 ]
