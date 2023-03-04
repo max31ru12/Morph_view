ef231f01d9d6ee0b44130096c71903263ee88e01
max31ru12 <maxevg72@gmail.com>
2023-03-04 19:15:05 +0300

    added patient template and new style for banner

--- morph_view/patient/views.py
+++ morph_view/patient/views.py
@@ -3,4 +3,4 @@ from django.shortcuts import render
 
 # Create your views here.
 def patient_index(request):
-    return render(request, 'patient/base.html')
+    return render(request, 'patient/patient.html')
